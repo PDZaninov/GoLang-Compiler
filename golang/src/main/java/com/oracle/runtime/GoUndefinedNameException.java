707cf7fe3e3c274f7b4de9a42b2d887447aebcc5
Petar Zaninovich <pzaninov@ucsc.edu>
2018-01-31 15:37:48 -0800

    added

---
+++ golang/src/main/java/com/oracle/runtime/GoUndefinedNameException.java
@@ -0,0 +1,23 @@
+package com.oracle.runtime;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.app.GoException;
+
+public final class GoUndefinedNameException extends GoException {
+
+    private static final long serialVersionUID = 1L;
+
+    @TruffleBoundary
+    public static GoUndefinedNameException undefinedFunction(Object name) {
+        throw new GoUndefinedNameException("Undefined function: " + name);
+    }
+
+    @TruffleBoundary
+    public static GoUndefinedNameException undefinedProperty(Object name) {
+        throw new GoUndefinedNameException("Undefined property: " + name);
+    }
+
+    private GoUndefinedNameException(String message) {
+        super(message);
+    }
+}
