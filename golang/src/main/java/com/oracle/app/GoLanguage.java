f0426572ba25314e526b44bf0ee90779e9ba314e
Trevor Ching <ttching@ucsc.edu>
2018-01-31 12:09:11 -0800

    Added a lot of boilerplate stuff and hopefully something that calls the parse function

---
+++ golang/src/main/java/com/oracle/app/GoLanguage.java
@@ -0,0 +1,55 @@
+package com.oracle.app;
+
+import com.oracle.runtime.GoContext;
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.frame.Frame;
+import com.oracle.truffle.api.metadata.ScopeProvider;
+import com.oracle.truffle.api.nodes.Node;
+
+@TruffleLanguage.Registration(id = "go", name = "GO",version = "1.6.2", mimeType = GoLanguage.MIME_TYPE)
+public final class GoLanguage extends TruffleLanguage<GoContext> implements ScopeProvider<GoContext>{
+
+	public static volatile int counter;
+	
+	public static final String MIME_TYPE = "application/x-go";
+	
+	public GoLanguage(){
+		counter++;
+	}
+	
+	@Override
+	public AbstractScope findScope(GoContext langContext, Node node,
+			Frame frame) {
+		//Returns something to do with lexical scope
+		return null;
+	}
+
+	@Override
+	protected GoContext createContext(Env env) {
+		return new GoContext(this, env);                    
+	}
+	
+	/*
+	 * Call the parse function here. Set the calltarget to the rootnode found
+	 * while parsing then return it
+	 */
+	@Override
+	protected CallTarget parse(ParsingRequest request) throws Exception{
+		System.out.println("PARSE FUNC");
+		return null;
+	}
+
+	@Override
+	protected Object getLanguageGlobal(GoContext context) {
+		//Might need to look into this. Something to do with global variables
+		return context;
+	}
+
+	@Override
+	protected boolean isObjectOfLanguage(Object object) {
+		//Returns whether or not the object is a TruffleObject
+		return false;
+	}
+
+}
