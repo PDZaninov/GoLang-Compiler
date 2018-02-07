6da0434ce1ca0ec974cb2b694b70a9ae2b3a9253
Petar Zaninovich <pzaninov@ucsc.edu>
2018-02-07 14:34:02 -0800

    adding/moving stuff

---
+++ golang/src/main/java/com/oracle/app/nodes/SpecDecl/GoDecl.java
@@ -0,0 +1,9 @@
+package com.oracle.app.nodes;
+
+import com.oracle.truffle.api.nodes.Node.Children;
+
+public class GoDecl {
+
+	@Children GoGenDecl[] genDecl;
+	//@Children GoFuncDecl[] genDecl;
+}
