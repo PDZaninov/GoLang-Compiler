09fc16a41842b800fcf19f1661d9123c90735d07
Petar Zaninovich <pzaninov@ucsc.edu>
2018-02-07 16:08:09 -0800

    some changes

--- golang/src/main/java/com/oracle/app/nodes/GoIdentNode.java
+++ golang/src/main/java/com/oracle/app/nodes/GoIdentNode.java
@@ -1,5 +1,20 @@
 package com.oracle.app.nodes;
 
-public class GoIdentNode {
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+public class GoIdentNode extends GoExpressionNode{
+
+	@Override
+	public Object executeGeneric(VirtualFrame frame) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
+//type Ident struct {
+//    NamePos token.Pos // identifier position
+//    Name    string    // identifier name
+//    Obj     *Object   // denoted object; or nil
+//}
