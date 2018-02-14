63f78ed193f67489fd9f65172f4c907436b2c244
Petar Zaninovich <pzaninov@ucsc.edu>
2018-02-13 23:48:12 -0800

    idk what hapend

---
+++ golang/src/test/java/com/oracle/app/ComparisonsTest.java
@@ -0,0 +1,42 @@
+package com.oracle.app;
+
+import java.io.IOException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ComparisonsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ComparisonsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ComparisonsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     * @throws IOException 
+     */
+    public void testApp() throws IOException
+    {	String[] args = new String[1];
+		args[0] = "comparison.ast";
+        GoMain.main(args);
+    }
+}
