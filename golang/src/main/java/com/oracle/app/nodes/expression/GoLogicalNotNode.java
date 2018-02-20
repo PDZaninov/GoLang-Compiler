17889e9105b81de444d5c3cd1f262389d0091fb2
Petar Zaninovich <pzaninov@ucsc.edu>
2018-02-20 12:02:25 -0800

    && not working, != seems to be working

--- golang/src/main/java/com/oracle/app/nodes/expression/GoLogicalNotNode.java
+++ golang/src/main/java/com/oracle/app/nodes/expression/GoLogicalNotNode.java
@@ -4,6 +4,7 @@ import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.app.nodes.GoExpressionNode;
+import com.oracle.app.nodes.GoUnaryNode;
 
 /**
  * Example of a simple unary node that uses type specialization. See {@link SLAddNode} for
@@ -11,7 +12,7 @@ import com.oracle.app.nodes.GoExpressionNode;
  */
 @NodeChild("valueNode")
 @NodeInfo(shortName = "!")
-public abstract class GoLogicalNotNode extends GoExpressionNode {
+public abstract class GoLogicalNotNode extends GoUnaryNode {
 
     @Specialization
     protected boolean doBoolean(boolean value) {
--- golang/src/main/java/com/oracle/app/nodes/expression/GoNotEqualNode.java
+++ golang/src/main/java/com/oracle/app/nodes/expression/GoNotEqualNode.java
@@ -12,32 +12,32 @@ import com.oracle.truffle.api.nodes.NodeInfo;
 @NodeInfo(shortName = "!=")
 public abstract class GoNotEqualNode extends GoBinaryNode {
     @Specialization
-    protected boolean equal(long left, long right) {
+    protected boolean notEqual(long left, long right) {
         return left != right;
     }
     @Specialization
-    protected boolean equal(int left, int right) {
+    protected boolean  notEqual(int left, int right) {
         return left != right;
     }
 
     @Specialization
     @TruffleBoundary
-    protected boolean equal(BigInteger left, BigInteger right) {
+    protected boolean  notEqual(BigInteger left, BigInteger right) {
         return !left.equals(right);
     }
 
     @Specialization
-    protected boolean equal(boolean left, boolean right) {
+    protected boolean  notEqual(boolean left, boolean right) {
         return left != right;
     }
 
     @Specialization
-    protected boolean equal(String left, String right) {
+    protected boolean  notEqual(String left, String right) {
         return !left.equals(right);
     }
 
     @Specialization
-    protected boolean equal(GoFunction left, GoFunction right) {
+    protected boolean  notEqual(GoFunction left, GoFunction right) {
         /*
          * Our function registry maintains one canonical SLFunction object per function name, so we
          * do not need equals().
@@ -46,7 +46,7 @@ public abstract class GoNotEqualNode extends GoBinaryNode {
     }
 
     @Specialization
-    protected boolean equal(GoNull left, GoNull right) {
+    protected boolean  notEqual(GoNull left, GoNull right) {
         /* There is only the singleton instance of SLNull, so we do not need equals(). */
         return left != right;
     }
