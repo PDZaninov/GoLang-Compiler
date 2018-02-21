2cb4820fe0e2677a3c5d8f08782e50d78c65e0f4
Petar Zaninovich <pzaninov@ucsc.edu>
2018-02-21 13:06:23 -0800

    fixed logicalOr test

--- golang/printast.go
+++ golang/printast.go
@@ -17,8 +17,12 @@ package main
 
 func main() {
 
-	println("true")
-	println(true)
+	println("1>0||2>1")
+	println(1>0||2>1)
+	println("0>0||2>1")
+	println(0>0||2>1)
+	println("0>0||0>1")
+	println(0>0||0>1)
 
 }
 `
@@ -32,7 +36,7 @@ func main() {
 
 	// Print the AST.
 	// ast.Print(fset, f)
-	f2, err2 := os.Create("PrintTrue.ast")
+	f2, err2 := os.Create("LogicalOr.ast")
 	if err2 != nil {
 		panic(err2)
 	}
