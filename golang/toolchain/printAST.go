7e2a01b248302e7bc86fff2de7f7b1c38c3359d6
Baiwen Huang <bhuang21@ucsc.edu>
2018-02-23 02:38:45 -0800

    added printAST.go to natively print the specified Go AST tree

---
+++ golang/toolchain/printAST.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// 	input := `
+	// package main
+	// func main() {
+	// 	println("Hello, World!")
+	// }
+	// `
+	file := os.Args[0]
+	b, err := ioutil.ReadFile(file) //reads File
+	// Create the AST by parsing src.
+	fset := token.NewFileSet()                         // positions are relative to fset
+	f, err := parser.ParseFile(fset, "", string(b), 0) //second input is file name
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Print(fset, f)
+
+}
