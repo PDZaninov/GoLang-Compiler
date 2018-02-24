99055806a18801d87372ff6f2928f5052d42620f
kremmert <kremmert25@gmail.com>
2018-02-24 11:45:42 -0800

    got rid of TruffleIR

--- golang/src/main/java/com/oracle/app/parser/ir/GoTruffle.java
+++ golang/src/main/java/com/oracle/app/parser/ir/GoTruffle.java
@@ -2,10 +2,11 @@ package com.oracle.app.parser.ir;
 
 import com.oracle.app.parser.ir.nodes.GoIRBasicLitNode;
 import com.oracle.app.parser.ir.nodes.GoIRBinaryExprNode;
+import com.oracle.app.parser.ir.nodes.GoIRGenericDispatchNode;
 import com.oracle.app.parser.ir.nodes.GoIRIdentNode;
 import com.oracle.app.parser.ir.nodes.GoIRInvokeNode;
 
-public class GoTruffle implements GoTruffleVisitor {
+public class GoTruffle implements GoIRVisitor {
 
 	public GoTruffle() {
 		
@@ -35,6 +36,12 @@ public class GoTruffle implements GoTruffleVisitor {
 
 	@Override
 	public void visitInvoke(GoIRInvokeNode node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visitGenericDispatch(GoIRGenericDispatchNode node) {
 		// TODO Auto-generated method stub
 		
 	}
