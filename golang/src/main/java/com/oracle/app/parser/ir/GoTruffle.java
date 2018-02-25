950eb18545b0ebaafb7cb89fc2890644b01e520c
kremmert <kremmert25@gmail.com>
2018-02-24 20:43:04 -0800

    added hashmap to gotruffle

--- golang/src/main/java/com/oracle/app/parser/ir/GoTruffle.java
+++ golang/src/main/java/com/oracle/app/parser/ir/GoTruffle.java
@@ -1,5 +1,11 @@
 package com.oracle.app.parser.ir;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.oracle.app.GoLanguage;
+import com.oracle.app.nodes.GoIdentNode;
+import com.oracle.app.nodes.GoRootNode;
 import com.oracle.app.parser.ir.nodes.GoIRArrayListExprNode;
 import com.oracle.app.parser.ir.nodes.GoIRBasicLitNode;
 import com.oracle.app.parser.ir.nodes.GoIRBinaryExprNode;
@@ -10,21 +16,35 @@ import com.oracle.app.parser.ir.nodes.GoIRFuncDeclNode;
 import com.oracle.app.parser.ir.nodes.GoIRGenericDispatchNode;
 import com.oracle.app.parser.ir.nodes.GoIRIdentNode;
 import com.oracle.app.parser.ir.nodes.GoIRInvokeNode;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.source.Source;
 
 public class GoTruffle implements GoIRVisitor {
 
-	public GoTruffle() {
-		
-	}
+	GoLanguage language;
+	
+    private final Source source;
+    private final Map<String, GoRootNode> allFunctions;
+    private FrameDescriptor frameDescriptor;
+	
+	public GoTruffle(GoLanguage language, Source source) {
+		this.language = language;
+		this.source = source;
+        this.allFunctions = new HashMap<>();
+    }
+
+    public Map<String, GoRootNode> getAllFunctions() {
+        return allFunctions;
+    }
 
 	@Override
 	public void visitObject(GoBaseIRNode node) {
-		// TODO Auto-generated method stub
+		System.out.println(node.toString());
 	}
 
 	@Override
 	public void visitIdent(GoIRIdentNode node) {
-		// TODO Auto-generated method stub
+		//GoIdentNode n = new GoIdentNode(language, node.getIdent(), node.getChild());
 	}
 
 	@Override
