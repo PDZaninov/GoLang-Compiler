522ba280ca8280c203b66950698ef837226aa103
Petar Zaninovich <pzaninov@ucsc.edu>
2018-03-06 23:48:47 -0800

    restoring files

---
+++ golang/src/test/java/com/oracle/app/LogicalAndTest.java
@@ -0,0 +1,42 @@
+package com.oracle.app;
+
+import java.io.IOException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LogicalAndTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LogicalAndTest ( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LogicalAndTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     * @throws IOException 
+     */
+    public void testApp() throws IOException
+    {	String[] args = new String[1];
+		args[0] = "LogicalAnd.ast";
+        GoMain.main(args);
+    }
+}
