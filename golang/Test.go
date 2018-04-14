cab46ca09dea1d2c4c409ff5095a4dffd8d46322
Trevor Ching <ttching@ucsc.edu>
2018-04-13 17:45:22 -0700

    Removed int string concatenation, cause it's not a thing. Now the default specialization on add nodes is only for stings.

--- golang/Test.go
+++ golang/Test.go
@@ -1,17 +1,9 @@
 package main
 
- 
-
 import "fmt"
 
- 
+func main(){
 
-var globalVar string = "saying hi"
-
- 
-
-func main() {
-
-            fmt.Println(globalVar)
-
+    fmt.Println("2" + "3")
+    
 }
