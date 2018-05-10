71ca39e481df117c87b71a1380679de8e5e0de00
Trevor Ching <ttching@ucsc.edu>
2018-05-09 19:16:12 -0700

    Created test file

--- golang/Test.go
+++ golang/Test.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-m := make(map[string]int)
-   m["k1"] = 7
-   m["k2"] = 13
-   fmt.Println("map:", m)
-   delete(m,"k1")
-   fmt.Println(m)
+type rect struct{
+width int
+height int
+}
 
+func (r *rect) area() int {
+    return r.width * r.height
+}
+
+func (r rect) perim() int {
+    return 2*r.width + 2*r.height
+}
+
+func main() {
+    r := rect {width: 10, height: 5}
+    
+    fmt.Println("area: ", r.area())
+    fmt.Println("perim: ", r.perim())
 }
