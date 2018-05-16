c4d282883b8ffe535da35e1d3b8d8882e181feca
Petar Zaninovich <pzaninov@ucsc.edu>
2018-05-16 12:43:47 -0700

    test

---
+++ golang/SliceTest.go
@@ -0,0 +1,27 @@
+package main
+
+
+func me() int {
+	  result := 0 
+   b := [5]int{1 , 2, 3, 4, 5} 
+	  s := b[1:3] 
+	  for j := 0; j < len(s); j++ { 
+		result += s[j] 
+	  } 
+	  s = b[:3] 
+	  for j := 0; j < len(s); j++ { 
+		result += s[j] 
+	  } 
+	  s = b[3:] 
+	  for j := 0; j < len(s); j++ { 
+		result += s[j] 
+	  } 
+	  s[0] = 10 
+	  for j := 0; j < len(s); j++ { 
+		result += s[j] 
+	  } 
+	  return result
+}
+func main(){ 
+	println(me())
+}
