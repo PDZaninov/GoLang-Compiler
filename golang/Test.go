5b15f51702f801a607fd08761a42f59c6695341e
Trevor Ching <ttching@ucsc.edu>
2018-05-23 22:32:32 -0700

    Fixed append bug. Append was not creating a new slice

--- golang/Test.go
+++ golang/Test.go
@@ -1,29 +1,18 @@
 package main
-			
-            
-			func main(){  
-				a := 1  
-				if a == 2 {  
-					a = 2  
-				} else if num := 10; num < 0 {  
-					a = 3  
-				} else {  
-					a = 4  
-				}  
-			 
-				i := 3  
-				for i <= 5 {  
-					a += 1  
-					i += 1  
-				}  
-			 
-				switch a {  
-					case 7:  
-						a = 100  
-					case 5:  
-						a = 50  
-					default:  
-						a = 10  
-					}  
-				println(a)  
-			}
+
+import "fmt"
+
+func main(){
+
+    array := [5]int{1,2,3,4,5}
+    slice := array[:4]
+    s := append(slice,6,7,8,9)
+    x := append(s[2:5],slice[3:]...)
+    y := append(x,x[1:4]...)
+    fmt.Println("array = ", array)
+    fmt.Println("slice = ", slice)
+    fmt.Println("s = ", s)
+    fmt.Println("x = ", x)
+    fmt.Println("y = ", y)
+
+}
